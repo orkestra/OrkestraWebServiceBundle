1e3718af3a70cff8041a12261c31f267988a031f
Tyler Sommer <sommertm@gmail.com>
2013-10-26 10:35:23 -0700

    Added license

--- Controller/FilterRequestContentInterface.php
+++ Controller/FilterRequestContentInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OrkestraWebServiceBundle package.
+ *
+ * Copyright (c) Orkestra Community
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Orkestra\Bundle\WebServiceBundle\Controller;
 
 interface FilterRequestContentInterface
