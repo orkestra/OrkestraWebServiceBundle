884c1c2e29231c7ff9bef4f95d4cd2468c10f6b8
Tyler Sommer <sommertm@gmail.com>
2013-10-28 11:16:18 -0700

    Updated bundle build method to register WSSE security factory

--- OrkestraWebServiceBundle.php
+++ OrkestraWebServiceBundle.php
@@ -11,8 +11,17 @@
 
 namespace Orkestra\Bundle\WebServiceBundle;
 
+use Orkestra\Bundle\WebServiceBundle\DependencyInjection\Security\Factory\WsseFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OrkestraWebServiceBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
 }
