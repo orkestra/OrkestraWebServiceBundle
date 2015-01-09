9b9c40e80e225b6e1d40e87ad67054123bdcacb6
Tyler Sommer <sommertm@gmail.com>
2015-01-08 19:45:19 -0700

    Added exception listener

--- Listener/FilterRequestSubscriber.php
+++ Listener/FilterRequestSubscriber.php
@@ -13,9 +13,12 @@ namespace Orkestra\Bundle\WebServiceBundle\Listener;
 
 use Negotiation\FormatNegotiator;
 use Orkestra\Bundle\WebServiceBundle\Controller\FilterRequestContentInterface;
+use Pocomos\Bundle\ApplicationBundle\Http\JsonResponse;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -46,13 +49,28 @@ class FilterRequestSubscriber implements EventSubscriberInterface
         $request->setRequestFormat($this->formatNegotiator->getBestFormat($request->headers->get('accept')));
     }
 
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ($event->getRequest()->getRequestFormat() !== 'json') {
+            return;
+        }
+
+        $exception = $event->getException();
+        if ($exception instanceof HttpExceptionInterface) {
+            $event->setResponse(new JsonResponse(null, $exception->getStatusCode()));
+        }
+
+        $event->setResponse(new JsonResponse(null, 500));
+    }
+
     /**
      * @return array
      */
     public static function getSubscribedEvents()
     {
         return array(
-            KernelEvents::REQUEST => 'onKernelRequest'
+            KernelEvents::REQUEST => 'onKernelRequest',
+            KernelEvents::EXCEPTION => 'onKernelException'
         );
     }
 }
